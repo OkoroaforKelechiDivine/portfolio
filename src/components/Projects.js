5152ff164b7e82ae3e198aad7ae546192273f6b6
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-11-02 10:35:37 +0100

    fixed issues

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -54,24 +54,27 @@ const Projects = () => {
                             {({ isVisible }) =>
                                 <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                                     <h2>Projects</h2>
-                                    <p>Strong in design and integration with intuitive problem-solving skills.
-                                        Proficient in JAVA, C#, PYTHON, JAVASCRIPT, and SQL. Passionate about implementing
-                                        and launching new projects. Ability to translate business requirements into
-                                        technical solutions.
+                                    <p> Strong in design and integration with intuitive problem-solving
+                                        skills. Proficient in JAVA, HTML, CSS, JAVASCRIPT, and NoSQL.
+                                        Passionate about implementing and launching new projects. Ability
+                                        to translate business requirements into technical solutions.
                                     </p>
                                     <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                                        <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                                        <Nav variant="pills"
+                                             className="nav-pills mb-5 justify-content-center align-items-center"
+                                             id="pills-tab">
                                             <Nav.Item>
                                                 <Nav.Link eventKey="first">Tab 1</Nav.Link>
                                             </Nav.Item>
-                                            <Nav.Item>
-                                                <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                                            </Nav.Item>
-                                            <Nav.Item>
-                                                <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                                            </Nav.Item>
+                                            {/*<Nav.Item>*/}
+                                            {/*    <Nav.Link eventKey="second">Tab 2</Nav.Link>*/}
+                                            {/*</Nav.Item>*/}
+                                            {/*<Nav.Item>*/}
+                                            {/*    <Nav.Link eventKey="third">Tab 3</Nav.Link>*/}
+                                            {/*</Nav.Item>*/}
                                         </Nav>
-                                        <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                                        <Tab.Content id="slideInUp"
+                                                     className={isVisible ? "animate__animated animate__slideInUp" : ""}>
                                             <Tab.Pane eventKey="first">
                                                 <Row>
                                                     {
@@ -86,12 +89,6 @@ const Projects = () => {
                                                     }
                                                 </Row>
                                             </Tab.Pane>
-                                            <Tab.Pane eventKey="section">
-                                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                                            </Tab.Pane>
-                                            <Tab.Pane eventKey="third">
-                                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                                            </Tab.Pane>
                                         </Tab.Content>
                                     </Tab.Container>
                                 </div>}
@@ -103,5 +100,4 @@ const Projects = () => {
         </section>
     )
 }
-
 export default Projects
