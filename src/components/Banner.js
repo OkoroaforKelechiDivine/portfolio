c54f5a27c2a56cd6f8290970015e117b90ef48a7
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-11-09 21:49:51 +0100

    fixed issues

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,6 @@ import headerImg from "../assets/img/header-img.svg";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
-import {type} from "@testing-library/user-event/dist/type";
 
 const Banner = () => {
     const [ loopNum, setLoopNum ] = useState(0);
