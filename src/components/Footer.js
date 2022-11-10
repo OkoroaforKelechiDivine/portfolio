2a3cb71b708affd7835361141c6ee3cb1fc4e4cc
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-11-10 22:57:19 +0100

    fixed issues

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,13 @@
 import { Container, Row, Col } from "react-bootstrap";
-import MailchimpForm from "./MailchimpForm";
 import { Facebook, Github, Linkedin, Medium, Twitter } from "react-bootstrap-icons";
 
 const Footer = () => {
     return (
         <footer className={"footer"}>
             <Container>
-                <Row className={"align-items-center"}>
-                    <MailchimpForm />
+                <Row className={"align-items-center mt-4"}>
                     <Col size={ 12 } sm={ 6 }>
                         <h1>zipDemon</h1>
-                        {/*<img src={logo} alt={"Logo"} />*/}
                     </Col>
                     <Col size={ 12 } sm={ 6 } className={"text-center text-sm-end"}>
                         <div className={"social-icon"}>
